c2acf618decd5d3c8aff9e04e0d7c9a9beaf1f19
ng3ksplo1t <87725499+ng3ksplo1t@users.noreply.github.com>
2022-02-28 13:04:54 +0700

    Create g.php

---
+++ g.php
@@ -0,0 +1,69 @@
+
+
+<html>
+
+<head>
+
+<title>./PWND By GalangGans</title>
+
+<link rel="shorcut icon" 
+
+<embed src="https://theelegancia.com/0.mp3" type="audio/mpeg" autostart="true" loop="true" height="0" width="0">
+
+<meta property="og:image" content="https://i.pinimg.com/originals/46/af/4b/46af4b9eebccc1e28bece08464ad7e38.jpg"
+
+href="https://i.pinimg.com/originals/46/af/4b/46af4b9eebccc1e28bece08464ad7e38.jpg">
+
+<meta name="description" content="scary in darkness.">
+
+<meta name="keyword" content="l0c4lh34dz, hacked by l0c4lh34dz">
+
+
+
+<base target="s_blank"/>
+
+
+
+<style type="text/css">
+
+.[ { color:red; }
+
+</style>
+
+
+
+<style type="text/css">
+
+body { font-family: Arial; }
+
+h1,h2,h3{ font-family: Courier, monospace; }
+
+
+
+</style>
+
+</head>
+
+
+
+<body oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;">
+
+<center>
+
+<h2>Hacked by GalangGans</h2>
+
+<img src="https://i.ibb.co/sCW89SL/BEWAo4b.png" height="300" title=":p"</p>
+
+<h3 class="[">[ KETIKA HITAM MULAI MEMUDAR. DAN PUTIH MULAI TERKONTAMINASI ]</h3>
+
+<h3>Indonesia Defacer Tersakiti - Owl Squad - Sanjungan Jiwa - IndoXploit Coders Team</h3>
+
+<h3>vend3r - l0c4lh34rtz - Wonka</h3>
+
+<embed src="https://www.youtube.com/v/B4jSt0ne1ck&amp;autoplay=1" type="application/x-
+
+
+
+shockwave-flash" wmode="transparent" width="1" height="1">
+
+</center></html>
